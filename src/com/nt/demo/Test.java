2a2f3b33eee52a1663df62ecbbc1d9b43172bd1b
hp <hp@DESKTOP-RKDAC7S>
2023-10-09 21:37:22 +0530

    First commit---

---
+++ src/com/nt/demo/Test.java
@@ -0,0 +1,14 @@
+package com.nt.demo;
+
+public class Test {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		if(a>b) {
+			System.out.println("a is bigger than b");
+		}else {
+			System.out.println("b is bigger tha a");
+		}
+	}
+
+}
