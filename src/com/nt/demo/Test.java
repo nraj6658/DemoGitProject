fea7a2988b29c50e42b0a238ec71a3d947f9600c
hp <hp@DESKTOP-RKDAC7S>
2023-10-09 21:42:19 +0530

    third commit---

--- src/com/nt/demo/Test.java
+++ src/com/nt/demo/Test.java
@@ -7,8 +7,10 @@ public class Test {
 		int b=20;
 		if(a>b) {
 			System.out.println("a is bigger than b");
-		}else {
+		}else if(b>a){
 			System.out.println("b is bigger tha a");
+		}else {
+			System.out.println("both are equal");
 		}
 	}
 
